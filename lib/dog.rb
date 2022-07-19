fb4acef838de5f8680da49d0faa88d1042acabf6
jordan <jordjamestaylor@gmail.com>
2022-07-19 11:54:09 -0400

    commit tree 8a20e401a369b634080d0b53ef84509829f30ef9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,29 @@
 class Dog
+    attr_accessor :name, :breed
 
+    def initialize(name="Rocky", breed="I dunno")
+        @name = name
+        @breed = breed
+    end
+    
+    # Don't need this method when using attr_write/attr_reader/attr_accessor
+    def name   
+       @name
+    end
+
+    # Don't need this method when using attr_write/attr_reader/attr_accessor
+    def breed
+        @breed
+    end
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def breed=(new_breed)
+        @breed = new_breed
+    end
 end
+
+dog1 = Dog.new("Cookie", "I dunno")
+dog1.name = "Winnie"
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
+    attr_accessor :name, :job
 
+    def initialize(name="John", job="Postman")
+        @name = name
+        @job = job
+    end
+
+    # Don't need this method when using attr_write/attr_reader/attr_accessor
+    def name
+        @name
+    end
+
+    # Don't need this method when using attr_write/attr_reader/attr_accessor
+    def job
+        @job
+    end
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def job=(new_job)
+        @job = new_job
+    end
 end
+
+me = Person.new("Jordan", "Fullstack Developer")
